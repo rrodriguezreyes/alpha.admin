0ae5708d62ae104692b8a8209aface401a42807e
Ricardo Rodriguez <r.rodriguezreyes@outlook.com>
2024-05-14 03:14:46 -0400

    Fix csf

--- alphaAdmin/settings.py
+++ alphaAdmin/settings.py
@@ -85,7 +85,7 @@ DATABASES = {
         'NAME': 'postgres',
         'USER': 'alpha',
         'PASSWORD': 'P@$$w0rd',
-        'HOST': 'alpha-dbserver.postgres.database.azure.com',
+        'HOST': 'localhost',
         'PORT': '5432',
         'OPTIONS': {
             'sslmode': 'require',
