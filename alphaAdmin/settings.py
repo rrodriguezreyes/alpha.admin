fb175cfda51ccbe626b4f7bb54a877a021d4871b
Ricardo Rodriguez <r.rodriguezreyes@outlook.com>
2024-05-14 02:24:18 -0400

    Fix csf

--- alphaAdmin/settings.py
+++ alphaAdmin/settings.py
@@ -41,7 +41,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'holidays',
-    'products'
+    'products',
+    'corsheaders'
 ]
 
 MIDDLEWARE = [
@@ -52,6 +53,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware'
 ]
 
 ROOT_URLCONF = 'alphaAdmin.urls'
@@ -137,3 +139,4 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+CSRF_TRUSTED_ORIGINS = ['https://alpha-adm.azurewebsites.net', 'https://www.alpha-adm.azurewebsites.net']
